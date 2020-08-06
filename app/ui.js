367f7c0e6cf01898577c4522f8520401f216c374
saumya <2saumyaray@gmail.com>
2020-08-06 09:58:03 +0530

    wip: stopping server

--- app/ui.js
+++ app/ui.js
@@ -3,12 +3,12 @@
 	//var os = require('os')
 	//document.write('Our computer is: ', os.platform())
 	//var osName = os.platform()
-
+/*
 	const express = require('express')
 	const app = express()
 	const port = 3000
 	const gui = require('nw.gui')
-
+*/
 	const onOpenDoctorApp = function(){
 		//gui.Window.open(`http://localhost:${port}`, {
 		gui.Window.open('https://doctor.findhealth.today/', { "width": 800, "height": 600 })
@@ -16,19 +16,41 @@
 	const onOpenPatientApp = function(){
 		gui.Window.open('https://letme.findhealth.today/', { "width": 1000, "height": 600 })
 	}
-
+/*
 	const startServer = function(){
 		console.log('startServer', new Date().toString())
 		app.get('/', (req, res) => res.send('Hello World!'))
 		app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 	}
+*/
 //
 //====================== JQuery ======================
 
 $(function(){
 	console.log('JQuery : Ready')
-	
+
 	var os = require('os')
 	let osName = os.platform()
 	$('#info').html(`OS: ${osName} `)
+
+	// Express
+	const express = require('express')
+	const app = express()
+	const port = 3000
+	const gui = require('nw.gui')
+
+	const onStartServer = ()=>{
+		console.log( 'onStartServer' )
+		app.get('/', (req, res) => res.send('Hello World!'))
+		app.listen(port, () => {
+			console.log(`Example app listening at http://localhost:${port}`)
+			$('#divServerStatus').html( `Server running at : http://localhost:${port}` ) 
+		})
+	}
+	const onStopServer = ()=>{
+		console.log('onStopServer')
+	}
+	//
+	$('#btnStartServer').on('click', onStartServer)
+	$('#btnStopServer').on('click', onStopServer)
 })
